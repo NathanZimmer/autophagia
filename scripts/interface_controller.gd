f9da0fd0a354b3048b8511c74426dd02a0b75c53
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2024-08-15 20:00:58 -0400

    added gdlint and gdformat, ran both

--- scripts/interface_controller.gd
+++ scripts/interface_controller.gd
@@ -3,42 +3,50 @@ extends CanvasLayer
 @export var show_fps: bool = true
 @export var show_b: bool = true
 
+var b_press_time_elapsed: float = 0
+
 @onready var fps_counter = $FPS_Counter
 @onready var b_counter = $BCounter
 @onready var pause_menu = $PauseMenuControl
 
-var b_press_time_elapsed: float = 0
 
 func _ready():
-    # DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-    pause_menu.hide()
+	# DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	pause_menu.hide()
 
-    if not show_fps:
-        fps_counter.hide()
-    if not show_b:
-        b_counter.hide()
+	if not show_fps:
+		fps_counter.hide()
+	if not show_b:
+		b_counter.hide()
+
 
 func _unhandled_input(event) -> void:
-    if event is InputEventKey:
-        if event.is_action_pressed('fullscreen'):
-            DisplayServer.window_set_mode(
-                DisplayServer.WINDOW_MODE_WINDOWED if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
-                else DisplayServer.WINDOW_MODE_FULLSCREEN
-            )
-        elif event.is_action_pressed('player_b'):
-            b_press_time_elapsed = 0
-        elif event.is_action_pressed('ui_cancel'):
-            if pause_menu.is_visible():
-                pause_menu.hide()
-            else:
-                pause_menu.show()
-    elif event is InputEventMouseButton and event.is_action_pressed('player_interact'):
-            print('left click')
+	if event is InputEventKey:
+		if event.is_action_pressed("fullscreen"):
+			DisplayServer.window_set_mode(
+				(
+					DisplayServer.WINDOW_MODE_WINDOWED
+					if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+					else DisplayServer.WINDOW_MODE_FULLSCREEN
+				)
+			)
+		elif event.is_action_pressed("player_b"):
+			b_press_time_elapsed = 0
+		elif event.is_action_pressed("ui_cancel"):
+			if pause_menu.is_visible():
+				pause_menu.hide()
+			else:
+				pause_menu.show()
+	elif event is InputEventMouseButton and event.is_action_pressed("player_interact"):
+		print("left click")
+
 
 func _process(delta):
-    if show_b:
-        b_press_time_elapsed += delta
-        b_counter.text = 'Seconds since the "B" key has been pressed: %d' % [int(b_press_time_elapsed)]
+	if show_b:
+		b_press_time_elapsed += delta
+		b_counter.text = (
+			'Seconds since the "B" key has been pressed: %d' % [int(b_press_time_elapsed)]
+		)
 
-    if show_fps:
-        fps_counter.text = str(Engine.get_frames_per_second())
+	if show_fps:
+		fps_counter.text = str(Engine.get_frames_per_second())
--- scripts/portal.gd
+++ scripts/portal.gd
@@ -2,9 +2,10 @@
 class_name Portal extends MeshInstance3D
 ## Mesh to be manipulated by a `PortalContainer`
 
-var shader_path = 'res://shaders/portal.gdshader'
+var shader_path = "res://shaders/portal.gdshader"
 var player_in_portal = false
 var on_screen = false
+
 
 ## Create portal shader object and pass in viewport texture [br]
 ## `viewport_texture`: The viewport to be displayed by the portal's `ShaderMaterial`
@@ -15,17 +16,20 @@ func set_material(viewport_texture: ViewportTexture) -> void:
 	material_override.resource_local_to_scene = true
 
 	# Assign ViewportTexture
-	material_override.set_shader_parameter('viewport_texture', viewport_texture)
-	material_override.set_shader_parameter('color', Vector3(1.0, 0.0, 0.0))
+	material_override.set_shader_parameter("viewport_texture", viewport_texture)
+	material_override.set_shader_parameter("color", Vector3(1.0, 0.0, 0.0))
+
 
 ## Delete the portal's `ShaderMaterial`
 func remove_material():
 	material_override = null
+
 
 ## Update the mesh and render layer of this portal [br]
 ## `box_mesh`: the new box mesh for this portal [br]
 ## `render_layer`: the render layer that this portal will render to
 func update_mesh(box_mesh: Mesh, render_layer: int) -> void:
 	mesh = box_mesh
-	for i in range(1, 21): set_layer_mask_value(i, false)
+	for i in range(1, 21):
+		set_layer_mask_value(i, false)
 	set_layer_mask_value(render_layer, true)
