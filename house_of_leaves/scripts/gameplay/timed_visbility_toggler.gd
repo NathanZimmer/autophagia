4e509476ab30ff10b579b95980151b15683c204a
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2024-09-29 19:15:27 -0400

    created ambulance model and scene, created visibility toggler, added ambulance to diner scene

---
+++ house_of_leaves/scripts/gameplay/timed_visbility_toggler.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+## How long the object stays hidden for
+@export var hide_duration: float = 0.5
+## How long the object is visible for
+@export var show_duration: float = 0.5
+
+var wait_start_time: float = 0
+var elapsed_time: float = 0
+
+func _process(delta):
+	elapsed_time += delta
+
+	if visible and show_duration - elapsed_time <= 0:
+		hide()
+		elapsed_time = 0
+	elif not visible and hide_duration - elapsed_time <= 0:
+		show()
+		elapsed_time = 0
