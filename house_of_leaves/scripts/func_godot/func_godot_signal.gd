4f412acd272a993c97125f572d07b58de1ce5954
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2024-10-03 22:41:28 -0400

    finished mapping introduction and programming door puzzle

--- house_of_leaves/scripts/func_godot/func_godot_signal.gd
+++ house_of_leaves/scripts/func_godot/func_godot_signal.gd
@@ -6,8 +6,8 @@ extends Area3D
 func _ready():
 	connect('body_entered', _emit_signal.bind())
 
-func _emit_signal(_body):
-	if not _body is CharacterBody3D:
+func _emit_signal(body):
+	if not body is CharacterBody3D:
 		return
 
 	Globals.emit_signal(func_godot_properties['signal'])
