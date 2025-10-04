f370dbce93401e180c3056bc760310ccf56e31b0
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2025-10-04 11:55:43 -0400

    re-implemented main menu scene from old commit (#60)
    * re-implemented main menu scene from old commit
    
    * merged assets and checked out master

---
+++ gui/main_menu_gui.gd
@@ -0,0 +1,14 @@
+extends Control
+## TODO
+
+
+func _shortcut_input(event: InputEvent) -> void:
+    if event is InputEventKey and event.is_action_pressed(InputActions.UI.FULLSCREEN):
+        _toggle_fullscreen()
+        accept_event()
+
+
+## Toggle window mode between `WINDOW_MODE_FULLSCREEN` and `WINDOW_MODE_WINDOWED`
+func _toggle_fullscreen() -> void:
+    var fullscreen := Overrides.load_fullscreen()
+    Overrides.save_fullscreen(!fullscreen)
---
+++ gui/menu/main_menu/main_menu.gd
@@ -0,0 +1,8 @@
+extends MenuControl
+## TODO
+
+# TODO: Add sub-menu for starting a new game or loading a save
+
+func _ready() -> void:
+    super._ready()
+    _back_button.pressed.connect(func() -> void: get_tree().quit())
