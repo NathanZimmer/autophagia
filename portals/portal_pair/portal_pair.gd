9a9e9a070c62e4e48af9c64aad8b20d8cae38902
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2025-11-26 14:22:01 -0500

    added some docstrings, fixed viewport activation logic edge cases

--- portals/portal_pair/portal_pair.gd
+++ portals/portal_pair/portal_pair.gd
@@ -20,10 +20,7 @@ var renderer_1: PortalRenderer
 var _child_pairs: Array[PortalPair]
 ## TODO
 var _secondary_target_map: Dictionary[PortalBody, Camera3D]
-## TODO
-var _skip_next_fp_deactivation := false
-## TODO
-var _skip_next_bp_deactivation := false
+var _pair_parent: PortalPair
 
 
 func _setup(portals: Array[PortalBody]) -> void:
@@ -36,35 +33,42 @@ func _setup(portals: Array[PortalBody]) -> void:
     _child_pairs.assign(find_children("*", "PortalPair", false))
 
     # Only want to start chain of commands if this is the root PortalPair
-    if is_instance_of(get_parent(), PortalPair):
+    var parent := get_parent()
+    if is_instance_of(parent, PortalPair):
+        _pair_parent = parent as PortalPair
         return
 
     ping_pong_layers(_forward_pass_render_layer, _back_pass_render_layer)
     set_viewport_activation_order()
     set_secondary_cameras()
-    connect_on_screen_enter()
+    connect_screen_enter_signals()
     connect_teleport_signals()
-    # TODO: Make viewport activation logic more robust and remove some of the edge case
-    # conditionals
     connect_viewport_activation_signals()
 
 
-## TODO
-func ping_pong_layers(layers: int, next_layers: int) -> void:
-    portal_0.render_layers = layers
-    portal_1.render_layers = layers
+## Recurse down the tree and set render layers and cull masks to such that each set of
+## portals can see their parent and children, but not themselves [br]
+## ## Parameters [br]
+## `render_layers`: render layers to use for this Node's portals. Passed in as
+## `cull_mask` when called in children [br]
+## `cull_mask`: cull mask for this Node's cameras. Passed in as `render_layers` when
+## called in children [br]
+func ping_pong_layers(render_layers: int, cull_mask: int) -> void:
+    portal_0.render_layers = render_layers
+    portal_1.render_layers = render_layers
 
-    renderer_0.cull_mask = _world_render_layers & ~layers
-    renderer_1.cull_mask = _world_render_layers & ~layers
+    renderer_0.cull_mask = _world_render_layers & ~render_layers
+    renderer_1.cull_mask = _world_render_layers & ~render_layers
     if not _child_pairs.is_empty() or true:
-        renderer_0.cull_mask |= next_layers
-        renderer_1.cull_mask |= next_layers
+        renderer_0.cull_mask |= cull_mask
+        renderer_1.cull_mask |= cull_mask
 
     for pair in _child_pairs:
-        pair.ping_pong_layers(next_layers, layers)
+        pair.ping_pong_layers(cull_mask, render_layers)
 
 
-## TODO
+## Recurse down the tree and set back pass viewport activation order. Activates root
+## viewport first.
 func set_viewport_activation_order() -> void:
     var sub_viewport := renderer_1.get_sub_viewport()
     RenderingServer.viewport_set_active(sub_viewport.get_viewport_rid(), false)
@@ -74,7 +78,8 @@ func set_viewport_activation_order() -> void:
         pair.set_viewport_activation_order()
 
 
-## TODO
+## Recurse down the tree and set secondary cameras for forward pass and back pass
+## renderers
 func set_secondary_cameras() -> void:
     for pair in _child_pairs:
         pair.renderer_0.secondary_target_cam = renderer_0.camera
@@ -82,15 +87,17 @@ func set_secondary_cameras() -> void:
         pair.set_secondary_cameras()
 
 
-## TODO
-func connect_on_screen_enter() -> void:
+## Recurse down the tree and connect `portal_entered_screen` signals to set camera usage
+## down the tree
+func connect_screen_enter_signals() -> void:
     portal_0.portal_entered_screen.connect(set_use_secondary_target.bind(false))
     portal_1.portal_entered_screen.connect(set_secondary_cam_from_map.bind(false, null))
     for pair in _child_pairs:
-        pair.connect_on_screen_enter()
+        pair.connect_screen_enter_signals()
 
 
-## TODO
+## Recurse down the tree and set special case camera usage signals for the frame that the
+## player teleports
 func connect_teleport_signals() -> void:
     portal_0.player_teleported.connect(
         func() -> void:
@@ -112,79 +119,87 @@ func connect_teleport_signals() -> void:
         pair.connect_teleport_signals()
 
 
-## TODO
+## Set Whether or not to use the secondary camera for the forward pass. Recurses down the
+## tree with `use_secondary_target = True` [br]
+## ## Parameters [br]
+## `use_secondary_target`: Whether `renderer_0` should use its secondary camera target
 func set_use_secondary_target(use_secondary_target: bool) -> void:
     renderer_0.set_target_cam(use_secondary_target)
     for pair in _child_pairs:
         pair.set_use_secondary_target(true)
 
 
-## TODO
+## Set Whether or not to use the secondary camera for the back pass. Signals up the tree
+## with `use_secondary_target = True, portal = self.portal_1` [br]
+## ## Parameters [br]
+## `use_secondary_target`: Whether `renderer_1` should use its secondary camera target [br]
+## `portal`: The portal to use when indexing `_secondary_target_map`
 func set_secondary_cam_from_map(use_secondary_target: bool, portal: PortalBody) -> void:
     renderer_1.secondary_target_cam = _secondary_target_map.get(portal)
     renderer_1.set_target_cam(use_secondary_target)
     back_pass_secondary_cam_updated.emit(true, portal_1)
 
 
-## TODO
+## Recurse down the tree and connect signals needed to enable/disable viewports
 func connect_viewport_activation_signals() -> void:
-    portal_0.portal_entered_screen.connect(set_down_chain_viewports_active.bind(true))
-    portal_0.portal_exited_screen.connect(set_down_chain_viewports_active.bind(false))
+    portal_0.portal_entered_screen.connect(set_down_chain_viewports_active)
+    portal_0.portal_exited_screen.connect(set_down_chain_viewports_active)
 
-    portal_1.portal_entered_screen.connect(set_up_chain_viewports_active.bind(true))
-    portal_1.portal_exited_screen.connect(set_up_chain_viewports_active.bind(false))
+    portal_1.portal_entered_screen.connect(set_up_chain_viewports_active)
+    portal_1.portal_exited_screen.connect(set_up_chain_viewports_active)
 
-    # Special behavior for viewport enabling/disabling after teleport
+    # Manually overriding these settings to prevent flicker
     portal_0.player_teleported.connect(
         func() -> void:
-            renderer_0.get_sub_viewport().render_target_update_mode = SubViewport.UPDATE_DISABLED
             renderer_1.get_sub_viewport().render_target_update_mode = (
                 SubViewport.UPDATE_WHEN_PARENT_VISIBLE
             )
-            skip_next_fp_deactivation()
     )
     portal_1.player_teleported.connect(
         func() -> void:
-            renderer_1.get_sub_viewport().render_target_update_mode = SubViewport.UPDATE_DISABLED
             renderer_0.get_sub_viewport().render_target_update_mode = (
                 SubViewport.UPDATE_WHEN_PARENT_VISIBLE
             )
-            _skip_next_bp_deactivation = true
-            for pair in _child_pairs:
-                pair.skip_next_fp_deactivation()
     )
 
     for pair in _child_pairs:
         pair.connect_viewport_activation_signals()
         pair.back_pass_cam_visibilty_changed.connect(set_up_chain_viewports_active)
-        pair.portal_0.player_teleported.connect(func() -> void: _skip_next_bp_deactivation = true)
 
 
 ## TODO
-func set_down_chain_viewports_active(active: bool, override_skip := false) -> void:
-    if _skip_next_fp_deactivation and not active and not override_skip:
-        _skip_next_fp_deactivation = false
-        return
+func set_down_chain_viewports_active(override_active :=false) -> void:
+    var active: bool = (
+        override_active
+        or portal_0._portal_on_screen
+        or _pair_parent and _pair_parent.fp_portal_on_screen()
+    )
     var render_target_update_mode := (
         SubViewport.UPDATE_WHEN_PARENT_VISIBLE if active else SubViewport.UPDATE_DISABLED
     )
     renderer_0.get_sub_viewport().render_target_update_mode = render_target_update_mode
     for pair in _child_pairs:
-        pair.set_down_chain_viewports_active(active, true)
+        pair.set_down_chain_viewports_active(active)
 
 
 ## TODO
-func set_up_chain_viewports_active(active: bool, override_skip := false) -> void:
-    if _skip_next_bp_deactivation and not active and not override_skip:
-        _skip_next_bp_deactivation = false
-        return
+func set_up_chain_viewports_active(override_active: bool = false) -> void:
+    var child_portal_1_on_screen := _child_pairs.any(
+        func(pair: PortalPair) -> bool: return pair.portal_1._portal_on_screen
+    )
+    var active: bool = (
+        override_active
+        or portal_1._portal_on_screen
+        or child_portal_1_on_screen
+    )
     var render_target_update_mode := (
         SubViewport.UPDATE_WHEN_PARENT_VISIBLE if active else SubViewport.UPDATE_DISABLED
     )
     renderer_1.get_sub_viewport().render_target_update_mode = render_target_update_mode
-    back_pass_cam_visibilty_changed.emit(active, true)
+
+    back_pass_cam_visibilty_changed.emit(active)
 
 
-## TODO
-func skip_next_fp_deactivation() -> void:
-    _skip_next_fp_deactivation = true
+## Returns `True` if the forward pass portal for this pair is on the screen
+func fp_portal_on_screen() -> bool:
+    return portal_0._portal_on_screen
