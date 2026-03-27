ff073d64cf2525452f1d00a70921b55dd21c1e0f
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2026-03-26 23:50:00 -0400

    Added selecting items from the inventory, updated inventory menu GUI with hard coded template

--- gui/menus/inventory_menu/inventory_icon.gd
+++ gui/menus/inventory_menu/inventory_icon.gd
@@ -1,18 +1,47 @@
 class_name iInventoryIcon extends Control
 ## TODO
 
+## TODO
+signal item_selected
+
+const HOVER_COLOR = Color(1.0, 0.66, 0.66)
+
 @onready var _name_label: Label = %NameLabel
 @onready var _count_label: Label = %CountLabel
 @onready var _icon_button: TextureButton = %IconButton
 
+var _item: ItemInfo
+
+
+func _ready() -> void:
+    _icon_button.pressed.connect(_on_press)
+    _icon_button.mouse_entered.connect(
+        func() -> void: _icon_button.modulate = HOVER_COLOR
+    )
+    _icon_button.mouse_exited.connect(
+        func() -> void: _icon_button.modulate = Color.WHITE
+    )
+    _on_depletion()
+
+
+func _on_press() -> void:
+    _icon_button.grab_focus()
+    item_selected.emit(_item)
+
 
 ## TODO
 func set_item(item: ItemInfo, count: int) -> void:
+    _item = item
     _name_label.text = item.name
+    _name_label.show()
     _count_label.text = str(count)
+    _count_label.show()
     _icon_button.texture_normal = item.icon
 
 
 ## TODO
 func _on_depletion() -> void:
-    pass
+    _name_label.hide()
+    _count_label.hide()
+    _icon_button.texture_normal = null
+    _item = null
--- gui/menus/inventory_menu/inventory_menu.gd
+++ gui/menus/inventory_menu/inventory_menu.gd
@@ -2,19 +2,35 @@ class_name iInventoryMenu extends iMenuControl
 ## TODO
 # TODO: Make the subviewport container work
 
+# TODO:
+    # - Make inventory size configurable
+    # - Update highlighting to not rely on Control node focus
+    # - Fix hard coding in inventory menu GUI
+    # - Add support for moving items, using items, and dropping items
+    # - Add support for moving items between inventory and containers
+
+
 var InventoryIcon := preload("uid://c4b0a3scm2jlc")
 var _inventory: Inventory
 
 @onready var _inventory_container: GridContainer = %InventoryContainer
 
+@onready var _name_label: Label = %NameLabel
+@onready var _desc_label: Label = %DescLabel
+@onready var _item_model: MeshInstance3D = %ItemModel
+
+@onready var _use_button: Button = %UseButton
+@onready var _move_button: Button = %MoveButton
+@onready var _drop_button: Button = %DropButton
+
 
 func _ready() -> void:
     super._ready()
+    _select_item(null)
 
 
-# FIXME: Make it so clicking buttons does not disable closing with tab
-func _input(event: InputEvent) -> void:
-    super._input(event)
+func _shortcut_input(event: InputEvent) -> void:
+    super._shortcut_input(event)
     if event is InputEventKey:
         if event.is_action_pressed(InputActions.UI.INVENTORY):
             menu_exited.emit()
@@ -39,6 +55,7 @@ func _init_inventory_container() -> void:
         child.queue_free()
     for i: int in range(_inventory.get_size()):
         var icon: iInventoryIcon = InventoryIcon.instantiate()
+        icon.item_selected.connect(_select_item)
         _inventory_container.add_child(icon)
 
     _update_inventory_container()
@@ -52,3 +69,26 @@ func set_inventory(inventory: Inventory) -> void:
     _inventory = inventory
     _inventory.updated.connect(_update_inventory_container)
     _init_inventory_container()
+
+
+## TODO
+func _select_item(item: ItemInfo) -> void:
+    if not item:
+        _name_label.hide()
+        _desc_label.hide()
+        _item_model.hide()
+        _use_button.hide()
+        _move_button.hide()
+        _drop_button.hide()
+        return
+    else:
+        _name_label.show()
+        _desc_label.show()
+        _item_model.show()
+        _use_button.show()
+        _move_button.show()
+        _drop_button.show()
+
+    _name_label.text = item.name
+    _desc_label.text = item.description
+    _item_model.mesh = item.mesh
